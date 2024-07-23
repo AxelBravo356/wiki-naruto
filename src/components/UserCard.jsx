20938bfd9307b9d2f7da9fbcc2767c2abfd7a693
Axl Bravo <axlbravo365@gmail.com>
2024-07-23 14:18:09 -0600

    Cambio de navigate al hacer onClick en los personajes

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,10 +1,10 @@
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export function UserCard({ character }) {
-  // const navigate = useNavigate();
-  const handleClick = () => {
-    window.open(`/character/${character.id}`, "_blank", "noopener,noreferrer");
-  };
+  const navigate = useNavigate();
+  // const handleClick = () => {
+  //   window.open(`/character/${character.id}`, "_blank", "noopener,noreferrer");
+  // };
 
   const imageSrc = character.images[0]
     ? character.images[0]
@@ -13,8 +13,8 @@ export function UserCard({ character }) {
   return (
     <div
       className="bg-zinc-800 p-3 hover:bg-zinc-700 hover:cursor-pointer rounded-lg"
-      // onClick={() => navigate(`/character/${character.id}`)}
-      onClick={handleClick}
+      onClick={() => navigate(`/character/${character.id}`)}
+      // onClick={handleClick}
     >
       <img
         className="object-contain h-48 w-96 mx-auto  p-5"
